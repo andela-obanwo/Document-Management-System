e37c8156edfa0071f7693a7062266bc5dcd625fa
Oreoluwa Banwo <oreoluwa.banwo@andela.com>
2017-02-28 16:49:06 +0100

    test: finish accesstypes spec file

--- server/tests/AccessTypesSpec.js
+++ server/tests/AccessTypesSpec.js
@@ -6,36 +6,32 @@ import db from '../models';
 
 const request = Request.agent(app);
 const expect = chai.expect;
-// console.log(app);
 const superAdmin = testData.superAdminUser;
 const departmentAdmin = testData.departmentAdminUser1;
 
-console.log(superAdmin.email, superAdmin.password);
-
-let superAdminToken, departmentAdminToken, userOneToken, userTwoToken;
+let superAdminToken, departmentAdminToken;
 describe('Access Type Tests', () => {
+  before((done) => {
+    db.sequelize.sync({ force: true })
+    .then(() => done());
+  });
   before((done) => {
     db.Roles.bulkCreate([testData.superAdminRole, testData.departmentAdminRole])
     .then(() => {
-      console.log('Roles Created');
       db.Departments.bulkCreate([
         testData.department1,
         testData.department2,
         testData.department3
       ])
       .then(() => {
-        console.log('Departments Created');
-        console.log(superAdmin, departmentAdmin);
         db.Users.bulkCreate([
           superAdmin,
           departmentAdmin
         ], { individualHooks: true })
         .then(() => {
-          console.log('Administrators Created');
           request.post('/users/login')
           .send(superAdmin)
           .end((err, res) => {
-            console.log('+++++++++++++++=======', res.body.token)
             superAdminToken = res.body.token;
             done();
           });
@@ -47,27 +43,22 @@ describe('Access Type Tests', () => {
     request.post('/users/login')
     .send(departmentAdmin)
     .end((err, res) => {
-      console.log(err, departmentAdmin);
-      console.log('from beforeerererer', err, res.body);
       departmentAdminToken = res.body.token;
       done();
     });
   });
 
-  after((done) => {
-    db.Users.destroy({ where: {} })
-    .then(() => {
-      db.Departments.destroy({ where: {} })
-      .then(() => {
-        db.Roles.destroy({ where: {} })
-        .then(() => {
-          db.AccessTypes.destroy({ where: {} });
-          done();
-        })
+  describe('Test Default route', () => {
+    it('should check that a welcome message is returned', (done) => {
+      request.get('/')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message)
+        .to.equal('Document Management API, Welcome');
+        done();
       });
     });
   });
-
   describe('Create Access Type', () => {
     it('should check Access Type is successfully created', (done) => {
       request.post('/accesstypes')
@@ -125,9 +116,95 @@ describe('Access Type Tests', () => {
       request.get('/accesstypes')
       .set({ 'x-access-token': departmentAdminToken })
       .end((err, res) => {
-        expect(res.status).to.equal(403);
+        expect(res.status).to.equal(401);
         expect(res.body.message)
         .to.equal('You are unauthorized to access this route');
+        done();
+      });
+    });
+  });
+  describe('Edit Access Types', () => {
+    it('should check if access type exists', (done) => {
+      request.put('/accesstypes/2')
+      .set({ 'x-access-token': superAdminToken })
+      .send(testData.publicAccessType)
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body.message).to.equal('AccessType not found');
+        done();
+      });
+    });
+    it('should check if user is not Super Admin', (done) => {
+      request.put('/accesstypes/2')
+      .set({ 'x-access-token': departmentAdminToken })
+      .send(testData.publicAccessType)
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        expect(res.body.message)
+        .to.equal('You are unauthorized to access this route');
+        done();
+      });
+    });
+    it('should check for error handling', (done) => {
+      request.put('/accesstypes/water')
+      .set({ 'x-access-token': superAdminToken })
+      .send('testData.publicAccessType')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        done();
+      });
+    });
+    it('should check that access type is successfully edited', (done) => {
+      request.put('/accesstypes/1')
+      .set({ 'x-access-token': superAdminToken })
+      .send(testData.publicAccessType)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message)
+        .to.equal('Update Successful');
+        done();
+      });
+    });
+  });
+  describe('Delete Access Types', () => {
+    it('should check if access type exists', (done) => {
+      request.delete('/accesstypes/2')
+      .set({ 'x-access-token': superAdminToken })
+      .send(testData.publicAccessType)
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body.message).to.equal('AccessType not found');
+        done();
+      });
+    });
+    it('should check if user is not Super Admin', (done) => {
+      request.delete('/accesstypes/2')
+      .set({ 'x-access-token': departmentAdminToken })
+      .send(testData.publicAccessType)
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        expect(res.body.message)
+        .to.equal('You are unauthorized to access this route');
+        done();
+      });
+    });
+    it('should check for error handling', (done) => {
+      request.delete('/accesstypes/water')
+      .set({ 'x-access-token': superAdminToken })
+      .send('testData.publicAccessType')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        done();
+      });
+    });
+    it('should check that access type is successfully deleted', (done) => {
+      request.delete('/accesstypes/1')
+      .set({ 'x-access-token': superAdminToken })
+      .send(testData.publicAccessType)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message)
+        .to.equal('AccessType deleted successfully.');
         done();
       });
     });
